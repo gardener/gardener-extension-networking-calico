d2d108da4bd1e0c8afcb471cb5266a60aaaac899
Vladimir Nachev <vladimir.nachev@sap.com>
2020-07-24 10:46:04 +0300

    honor max-concurrent-reconciles flag

--- cmd/gardener-extension-networking-calico/app/app.go
+++ cmd/gardener-extension-networking-calico/app/app.go
@@ -89,6 +89,7 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 			}
 
 			reconcileOpts.Completed().Apply(&calicocontroller.DefaultAddOptions.IgnoreOperationAnnotation)
+			calicoCtrlOpts.Completed().Apply(&calicocontroller.DefaultAddOptions.Controller)
 			configFileOpts.Completed().ApplyHealthCheckConfig(&healthcheck.AddOptions.HealthCheckConfig)
 			healthCheckCtrlOpts.Completed().Apply(&healthcheck.AddOptions.Controller)
 
