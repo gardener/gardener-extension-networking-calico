b9c39265dd9a6195eb3984bcb55ab0230ffbc7c5
Johannes Scheerer <johannes.scheerer@sap.com>
2021-06-29 12:26:46 +0200

    Added validating admission webhook for calico networking extension.
    In case Shoot.Spec.Kubernetes.KubeProxy.Enabled flag is enabled the validating admission webhook will reject the request.
    Disabling kube-proxy is only possible with calico with the ebpf data plane, which is not yet supported by this extension.

---
+++ pkg/admission/validator/shoot.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validator
+
+import (
+	"context"
+	"fmt"
+
+	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
+	"github.com/gardener/gardener/pkg/apis/core"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// NewShootValidator returns a new instance of a shoot validator.
+func NewShootValidator() extensionswebhook.Validator {
+	return &shoot{}
+}
+
+type shoot struct {
+	client         client.Client
+	apiReader      client.Reader
+	decoder        runtime.Decoder
+	lenientDecoder runtime.Decoder
+}
+
+// InjectScheme injects the given scheme into the validator.
+func (s *shoot) InjectScheme(scheme *runtime.Scheme) error {
+	s.decoder = serializer.NewCodecFactory(scheme, serializer.EnableStrict).UniversalDecoder()
+	s.lenientDecoder = serializer.NewCodecFactory(scheme).UniversalDecoder()
+	return nil
+}
+
+// InjectClient injects the given client into the validator.
+func (s *shoot) InjectClient(client client.Client) error {
+	s.client = client
+	return nil
+}
+
+// InjectAPIReader injects the given apiReader into the validator.
+func (s *shoot) InjectAPIReader(apiReader client.Reader) error {
+	s.apiReader = apiReader
+	return nil
+}
+
+// Validate validates the given shoot object.
+func (s *shoot) Validate(ctx context.Context, new, old client.Object) error {
+	shoot, ok := new.(*core.Shoot)
+	if !ok {
+		return fmt.Errorf("wrong object type %T", new)
+	}
+
+	if old != nil {
+		oldShoot, ok := old.(*core.Shoot)
+		if !ok {
+			return fmt.Errorf("wrong object type %T for old object", old)
+		}
+		return s.validateShootUpdate(ctx, oldShoot, shoot)
+	}
+
+	return s.validateShootCreation(ctx, shoot)
+}
+
+func (s *shoot) validateShoot(_ context.Context, shoot *core.Shoot) error {
+	if shoot.Spec.Kubernetes.KubeProxy != nil {
+		if shoot.Spec.Kubernetes.KubeProxy.Enabled != nil {
+			if !*shoot.Spec.Kubernetes.KubeProxy.Enabled {
+				return field.Forbidden(field.NewPath("spec", "kubernetes", "kubeProxy", "enabled"), "Disabling kube-proxy is forbidden in conjunction with calico")
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *shoot) validateShootUpdate(ctx context.Context, oldShoot, shoot *core.Shoot) error {
+	return s.validateShoot(ctx, shoot)
+}
+
+func (s *shoot) validateShootCreation(ctx context.Context, shoot *core.Shoot) error {
+	return s.validateShoot(ctx, shoot)
+}
---
+++ pkg/admission/validator/webhook.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validator
+
+import (
+	"github.com/gardener/gardener-extension-networking-calico/pkg/calico"
+
+	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
+	"github.com/gardener/gardener/pkg/apis/core"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+var logger = log.Log.WithName("calico-validator-webhook")
+
+// New creates a new webhook that validates Shoot resources.
+func New(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
+	logger.Info("Setting up webhook", "name", extensionswebhook.ValidatorName)
+
+	return extensionswebhook.New(mgr, extensionswebhook.Args{
+		Provider:   calico.Name,
+		Name:       extensionswebhook.ValidatorName,
+		Path:       extensionswebhook.ValidatorPath,
+		Predicates: []predicate.Predicate{createCalicoPredicate()},
+		Validators: map[extensionswebhook.Validator][]client.Object{
+			NewShootValidator(): {&core.Shoot{}},
+		},
+	})
+}
+
+func createCalicoPredicate() predicate.Funcs {
+	f := func(obj client.Object) bool {
+		if obj == nil {
+			return false
+		}
+
+		shoot, ok := obj.(*core.Shoot)
+		if !ok {
+			return false
+		}
+
+		return shoot.Spec.Networking.Type == calico.ReleaseName
+	}
+
+	return predicate.Funcs{
+		CreateFunc: func(event event.CreateEvent) bool {
+			return f(event.Object)
+		},
+		UpdateFunc: func(event event.UpdateEvent) bool {
+			return f(event.ObjectNew)
+		},
+		GenericFunc: func(event event.GenericEvent) bool {
+			return f(event.Object)
+		},
+		DeleteFunc: func(event event.DeleteEvent) bool {
+			return f(event.Object)
+		},
+	}
+}
