52c322f631f5332c5f278c9483632fd868a3e3a1
Shafeeque E S <shafeeque.e.s@sap.com>
2023-03-21 09:07:37 +0530

    Add `ShootSystemComponentsHealthy` to `conditionTypesToRemove``
    Run `make revendor`

--- pkg/healthcheck/add.go
+++ pkg/healthcheck/add.go
@@ -19,15 +19,14 @@ import (
 
 	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
-	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"github.com/gardener/gardener-extension-networking-calico/pkg/calico"
-	networkcontroller "github.com/gardener/gardener-extension-networking-calico/pkg/controller"
 )
 
 var (
@@ -49,12 +48,8 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		mgr,
 		opts,
 		nil,
-		[]healthcheck.ConditionTypeToHealthCheck{
-			{
-				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
-				HealthCheck:   general.CheckManagedResource(networkcontroller.CalicoConfigSecretName),
-			},
-		},
+		[]healthcheck.ConditionTypeToHealthCheck{},
+		sets.New[gardencorev1beta1.ConditionType](gardencorev1beta1.ShootSystemComponentsHealthy),
 	)
 }
 
