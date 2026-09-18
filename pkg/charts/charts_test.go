7a2cc9466d624841468b6d39c2e8e6b0036d378b
Tobias Pfaffelmoser <tobias.pfaffelmoser_ext@external.digits.schwarz>
2026-09-18 14:40:31 +0200

    Update pkg/charts/charts_test.go
    Co-authored-by: Johannes Scheerer <johannes.scheerer@sap.com>
    Signed-off-by: Tobias Pfaffelmoser <tobias.pfaffelmoser_ext@external.digits.schwarz>

--- pkg/charts/charts_test.go
+++ pkg/charts/charts_test.go
@@ -780,7 +780,7 @@ var _ = Describe("Chart package test", func() {
 				return releaseutil.Manifest{Name: fmt.Sprintf("test/templates/%s", name), Content: testManifestContent}
 			}
 		})
-		Describe("kube-apiserver GlobalNetworkSet", func() {
+		Describe("gardener-kube-apiserver GlobalNetworkSet", func() {
 			renderChart := func(kubeAPIServerCIDRs []string) string {
 				renderer := chartrenderer.NewWithServerVersion(&version.Info{GitVersion: kubernetesVersion})
 
